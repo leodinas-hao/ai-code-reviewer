00b4dbafc5458d3676f56ae863fe81e30fe64a75
Leo Hao <lhao@macquarietelecom.com>
2023-11-08 13:49:49 +1100

    test: add code review test cases

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -4,6 +4,8 @@ import logging
 import pytest
 
 from ai_code_reviewer.utils import chat_openai, openai_client
+from ai_code_reviewer.gitlab import get_diffs, post_comment
+from ai_code_reviewer.code_review import review
 
 
 @pytest.mark.parametrize('prompt', [
@@ -34,3 +36,16 @@ def test_chat_openai(msgs):
   res = chat_openai(msgs)
   assert res.choices[0].message.content
   logging.getLogger(__name__).info(f'OpenAI response: {res.choices[0].message.content}')
+
+
+@pytest.mark.parametrize('pid, mr_id', [
+  (10, 1)
+])
+def test_git_diff_code_review(pid, mr_id):
+  res = get_diffs(pid, mr_id)
+  diffs = [change["diff"] for change in res["changes"]]
+  res = review(diffs)
+  assert res
+
+  res = post_comment(pid, mr_id, res)
+  assert res
